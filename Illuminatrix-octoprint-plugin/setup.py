aa15a01fbf6af4f087a99e84b3a563e2c61f1e26
docdawning <doc@dawning.ca>
2016-06-22 08:49:15 +0000

    Having insallation problems, trying things..

--- Illuminatrix-octoprint-plugin/setup.py
+++ Illuminatrix-octoprint-plugin/setup.py
@@ -11,10 +11,10 @@ plugin_package = "octoprint_%s" % plugin_identifier
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Illuminatrix"
+plugin_name = "Illuminatrix"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.2"
+plugin_version = "0.3"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
